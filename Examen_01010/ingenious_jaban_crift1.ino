005cdb631a4e05f9ec089f2da78b2e28c28d3a41
Kalh2020 <101225983+Kalh2020@users.noreply.github.com>
2022-09-09 11:09:39 -0300

    Entrega de examen
    Se entriqwmfodsn paokdsp oajdoasmdpoa mdoiasmd opajsdioa

---
+++ Examen_01010/ingenious_jaban_crift1.ino
@@ -0,0 +1,15 @@
+/* Con el potenciometro vamos a variar la tonalidad del color del 
+con el piezo va a sonar si el led se pone en rojo
+fotoresistencia: 
+
+
+*/ 
+void setup()
+{
+ 
+}
+
+void loop()
+{
+  
+}
